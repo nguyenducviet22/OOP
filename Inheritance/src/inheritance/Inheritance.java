750261694fb03f4f56c6aa1f3b61627e1f6c33cd
nguyenducviet22 <ndv6060@gmail.com>
2024-04-29 14:00:42 +0700

    update

---
+++ Inheritance/src/inheritance/Inheritance.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+import data.Rectangle;
+import data.Round;
+import data.Square;
+
+/**
+ *
+ * @author NGUYEN DUC VIET
+ */
+public class Inheritance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        cutShapes();
+//        sortArea();
+        Rectangle rec = new Rectangle("Dad", "Gray", 4, 5);
+        rec.showInfo();//bold black => rectangle-self
+        Square sq = new Square("Me", "Orange", 3);
+        sq.showInfo();//bold black => square-self
+        Rectangle recSq = new Square("Mom", "Blue", 4);
+        recSq.showInfo();
+        
+    }
+
+    public static void sortArea() {
+        Rectangle[] recArr = new Rectangle[4];
+
+        recArr[0] = new Rectangle("Dad", "Black", 2, 3);
+        recArr[1] = new Rectangle("Mom", "Blue", 3, 4);
+        recArr[2] = new Rectangle("Sis", "Red", 1, 4);
+        
+        recArr[3] = new Square("Bro", "Green", 6);
+        
+//        for (Rectangle rec : recArr) {
+//            rec.showInfo();
+//        }
+        
+        for (int i = 0; i < 4; i++) {
+            recArr[i].showInfo();
+        }
+        
+        for (int i = 0; i < 4 - 1; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                if (recArr[i].getArea() > recArr[j].getArea()) {
+                    Rectangle t = recArr[i];
+                    recArr[i] = recArr[j];
+                    recArr[j] = t;
+                }
+            }
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            recArr[i].showInfo();
+        }
+    }
+
+    public static void cutShapes() {
+        Rectangle rec = new Rectangle("Dad", "Red", 5, 10);
+        rec.showInfo();
+
+        Square sq = new Square("Mon", "Pink", 9);
+        sq.showInfo();
+
+        Round r = new Round("Bro", "Blue", 5);
+        r.showInfo();
+
+    }
+
+}
