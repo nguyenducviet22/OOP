4ced0359eb80e14382b642aa21bafc911a2044d6
nguyenducviet22 <ndv6060@gmail.com>
2024-05-03 10:27:13 +0700

    update 3/5

--- Abstract/src/data/Rectangle.java
+++ Abstract/src/data/Rectangle.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
-/**
- *
- * @author NGUYEN DUC VIET
- */
-public class Rectangle {
-    
+import pkgabstract.Shape;
+
+public class Rectangle extends Shape {
+
+    private double a, b;
+
+    public Rectangle(String name, String color, double a, double b) {
+        super(name, color);
+        this.a = a;
+        this.b = b;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" + "a=" + a + ", b=" + b + '}';
+    }
+
+    @Override
+    public double getArea() {
+        return a * b;
+    }
+
+    @Override
+    public double getPerimiter() {
+        return (a + b) * 2;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.printf("|Rectangle |%-10s|%-10s|%4.1f|%4.1f|%7.2f|%7.2f|\n", 
+                name, color, a, b, getArea(), getPerimiter());
+    }
+
 }
--- Abstract/src/pkgabstract/Shape.java
+++ Abstract/src/pkgabstract/Shape.java
@@ -9,6 +9,40 @@ package pkgabstract;
  *
  * @author NGUYEN DUC VIET
  */
-public class Shape {
-    
+public abstract class Shape {
+
+    protected String name;
+    protected String color;
+
+    public Shape(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" + "name=" + name + ", color=" + color + '}';
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimiter();
+
+    public abstract void showInfo();
 }
